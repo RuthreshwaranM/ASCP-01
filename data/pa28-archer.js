183a3d3171fef31cc0e9112e660f2603a0b210ab
RuthreshwaranM <wruthresh@gmail.com>
2026-09-26 08:33:09 +0530

    Update pa28-archer.js

--- data/pa28-archer.js
+++ data/pa28-archer.js
@@ -119,9 +119,9 @@ d. 6.5 bar (94 psi)
 [explanation-image: oilpx.png]
 
 16. Maximum Oil Pressure (cold start < 20 sec):
-*a. 2.3 bar (33.4 psi)
+a. 2.3 bar (33.4 psi)
 b. 1.2 bar (14.5 psi)
-c. 1 bar
+*c. 6.5 bar (94 psi)
 d. 6 bar (94 psi)
 [explanation-image: oilpx.png]
 
